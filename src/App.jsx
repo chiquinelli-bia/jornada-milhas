c7538aebbea0234a286d1657e176e696d1133e9b
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-05-08 08:14:06 -0300

    feat(NotificationButton): cria componente e importa

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import GlobalStyle from "./styles/global";
 import Home from "./pages/Home";
+import useNotification from "./hooks/useNotification";
+import NotificationButton from "./components/NotificationButton";
 
 function App() {
+  const {} = useNotification();
   return (
     <>
       <GlobalStyle />
+      <NotificationButton />
       <Home />
     </>
   );
