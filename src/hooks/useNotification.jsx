620ad705774dd181c6ba9718c5a914af38fbff57
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-05-18 08:03:13 -0300

    feat(useNotification): muda nome do hook para UseFirebaseNotification e adiciona listener de notificações em foreground

--- src/hooks/useNotification.jsx
+++
@@ -1,8 +0,0 @@
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-const useFirebaseNotification = () => {
-  return {};
-};
-
-export default useFirebaseNotification;
