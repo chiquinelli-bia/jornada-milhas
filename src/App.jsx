891a7cb027f0c114f22db51c3a4f8218256237cf
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-05-18 07:57:46 -0300

    feat(toast): cria e importa componente ToastDisplay para notificações personalizadas

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,29 @@ import GlobalStyle from "./styles/global";
 import Home from "./pages/Home";
 import NotificationButton from "./components/NotificationButton";
 import { requestToken } from "./firebase";
+import { ToastContainer } from "react-toastify";
+import useFirebaseNotification from "./hooks/useFirebaseNotification";
+import styled from "styled-components";
+
+const StyledToastContainer = styled(ToastContainer)`
+  .Toastify-toast {
+    font-size: 16px;
+    font-weight: 600;
+
+    &-theme--light {
+      background: #ffffff;
+      color: #5a189a;
+    }
+  }
+`;
 
 function App() {
+  useFirebaseNotification();
   return (
     <>
       <GlobalStyle />
+      <StyledToastContainer />
+
       <NotificationButton action={requestToken} />
       <Home />
     </>
